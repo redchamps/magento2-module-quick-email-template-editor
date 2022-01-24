92c46bbec4c90ec01bb62bb1a8e80541a7db7fe1
Sanchit <sanchit@redmonks.agency>
2022-01-24 17:24:06 +0530

    Added back button

--- Plugin/MagentoConfig/System/Form/Field.php
+++ Plugin/MagentoConfig/System/Form/Field.php
@@ -5,16 +5,28 @@ namespace RedMonks\QuickEmailTemplateEditor\Plugin\MagentoConfig\System\Form;
 
 use Magento\Backend\Model\UrlInterface;
 use Magento\Config\Block\System\Config\Form\Field as BaseField;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Url\EncoderInterface;
 
 class Field
 {
     /** @var UrlInterface */
     private $url;
 
+    /** @var RequestInterface */
+    private $request;
+
+    /** @var EncoderInterface */
+    private $encoder;
+
     public function __construct(
-        UrlInterface $url
+        UrlInterface $url,
+        RequestInterface $request,
+        EncoderInterface $encoder
     ) {
         $this->url = $url;
+        $this->request = $request;
+        $this->encoder = $encoder;
     }
 
     public function afterRender(BaseField $subject, $result, $element)
@@ -36,16 +48,34 @@ class Field
     private function appendHtml($label, $action, $result)
     {
         $result = str_replace('<select ', '<select style="width:85%;" ', $result);
-        return str_replace('</select>','</select><a class="action-default " href="'.$action.'" target="_blank">'.$label.'</a>', $result);
+        return str_replace('</select>','</select><a class="action-default " href="'.$action.'">'.$label.'</a>', $result);
     }
 
     private function getEditTemplateUrl($id)
     {
-        return $this->url->getUrl('adminhtml/email_template/edit',['id' => $id]);
+        return $this->url->getUrl('adminhtml/email_template/edit',['id' => $id, 'return_path' => $this->getCurrentPath()]);
     }
 
     private function getNewEmailTemplateUrl($templateId)
     {
-        return $this->url->getUrl('adminhtml/email_template/new',['template_id' => $templateId]);
+        return $this->url->getUrl('adminhtml/email_template/new',['template_id' => $templateId, 'return_path' => $this->getCurrentPath()]);
+    }
+
+    private function getCurrentPath()
+    {
+        $params = $this->request->getParams();
+        if (isset($params[UrlInterface::SECRET_KEY_PARAM_NAME])) {
+            unset($params[UrlInterface::SECRET_KEY_PARAM_NAME]);
+        }
+        $url = $this->request->getRouteName().'/'.$this->request->getControllerName().'/'.$this->request->getActionName().$this->makeParamString($params);
+        return $this->encoder->encode($url);
+    }
+    private function makeParamString($params)
+    {
+        $string = '';
+        foreach ($params as $key => $value) {
+            $string .= '/'.$key .'/'.$value;
+        }
+        return $string;
     }
 }
